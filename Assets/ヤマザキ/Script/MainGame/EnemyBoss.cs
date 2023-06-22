ee3298e3397095f456e8dfe71e927cb764f08469
Yamazaki <>
2023-06-23 02:49:43 +0900

    Bossのプレイヤー追う動き

--- Assets/ヤマザキ/Script/MainGame/EnemyBoss.cs
+++ Assets/ヤマザキ/Script/MainGame/EnemyBoss.cs
@@ -1,62 +1,235 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class EnemyBoss : MonoBehaviour
 {
     public Camera cam;
     public GameObject target;
     public GameObject test;
-
+    [SerializeField] private Camera targetCamera;
     private RectTransform rect;
-    //private GameObject textObject;
-    // Start is called before the first frame update
-
-    //Vector3 direction = new Vector3(0, 0, -1);
-    //Vector3 origwin = new Vector3(0, 3, 0);
 
     float distance = 100; // 飛ばす&表示するRayの長さ
     float duration = 3;   // 表示期間（秒）
+    bool GetAngry;//怒っているフラグ
 
+    // 巡回地点オブジェクトを格納する配列
+    [SerializeField] private Transform[] points;
+    [SerializeField] private Transform[] BranchpointsOne;
+    [SerializeField] private Transform[] BranchpointsTwo;
+    [SerializeField] private Transform[] BranchpointsThree;
+    [SerializeField] private Transform[] BranchpointsFour;
+    [SerializeField] private GameObject player;
+    // 巡回地点のオブジェクト数（初期値=0）
+    private int destPoint = 0;
+    private int bPoint = 0;
+    private float Chasetime;//巡回モードに戻るときに10秒間見られていないかの確認
+    private NavMeshAgent agent;// NavMesh Agent コンポーネントを格納する変数
+    private bool Branch;//分岐点を決める
+    private Enemy EnemyState;//敵の状態をENUMから引き出す関数
+
+
+    public enum Enemy
+    {
+        Patrol,//巡回
+        PlayerLook,//怒る
+        Frightening,//怯み
+        Capture//捕獲
+    }
 
 
     void Start()
     {
         rect = GetComponent<RectTransform>();
         //textObject = transform.Find("Text")?.gameObject;
+        // 変数"agent"に NavMesh Agent コンポーネントを格納
+        agent = GetComponent<NavMeshAgent>();
+        // 巡回地点間の移動を継続させるために自動ブレーキを無効化
+        //（エージェントは目的地点に近づいても減速しない)
+        agent.autoBraking = false;
+        // 次の巡回地点の処理を実行
+        GotoNextPoint();
+        //Branchpoint();
+        EnemyState = Enemy.Patrol;
     }
 
     void FixedUpdate()
     {
+        Debug.Log(EnemyState);
+        switch (EnemyState)
+        {
+            case Enemy.Patrol://巡回
+                Debug.Log("巡回");
+                if (!agent.pathPending && agent.remainingDistance < 0.05f)
+                {
+                    Debug.Log(Branch);
+                    // 次の巡回地点を設定する処理を実行
+                    if (!Branch)
+                    {
+                        GotoNextPoint();
+                    }
+                    else
+                    {
+                        GotoBranchPoint();
+                    }
+                }
+                break;
+            case Enemy.PlayerLook://怒る
+                Debug.Log("プレイヤー発見");
+                agent.destination = player.transform.position;
+                ChaseTime();
+                break;
+            case Enemy.Frightening://怯み
+                Debug.Log("プレイヤーアイテム使用");
+                break;
+            case Enemy.Capture://捕獲
+                Debug.Log("プレイヤーを捕まえた");
+                break;
+        }
+
         PLRay();
-        PVCamera();
 
     }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void PLRay()
     {
         Ray ray = new Ray(test.transform.position, test.transform.forward);
         Debug.DrawRay(ray.origin, ray.direction * distance, Color.red, duration, false);
         RaycastHit hit;
+
         if (Physics.Raycast(ray, out hit))
         {
-            Debug.Log(hit.point);
+            //Debug.Log(hit.point);
+        }
+        if (hit.collider.gameObject.CompareTag("Player"))
+        {
+            //もしhitのタグが"Player"と一致していた場合．．．の処理内容
+            EnemyState = Enemy.PlayerLook;
+            Chasetime = 0;
+
         }
     }
-    void PVCamera()
+    // 次の巡回地点を設定する処理
+    void GotoNextPoint()
     {
-        if (cam == null || target == null || rect == null)
-        {
+        // 巡回地点が設定されていなければ
+        if (points.Length == 0)
+            // 処理を返します
             return;
-        }
-        rect.position = cam.WorldToScreenPoint(target.transform.position) + new Vector3(0.0f, 10.0f, 0.0f);
-        var vp = cam.WorldToViewportPoint(target.transform.position);
-        bool active = vp.x >= 0.0f && vp.x <= 1.0f && vp.y >= 0.0f && vp.y <= 1.0f && vp.z >= 0.0f;
-        Debug.Log("写ってる");
+        // 現在選択されている配列の座標を巡回地点の座標に代入
+        agent.destination = points[destPoint].position;
+        // 配列の中から次の巡回地点を選択（必要に応じて繰り返し）
+        destPoint = (destPoint + 1) % points.Length;
+    }
+    void GotoBranchPoint()
+    {
 
+        switch (destPoint)
+        {
+            case 3:
+                destPoint += 4;
+                Branch = false;
+                break;
+            case 8:
+                if (BranchpointsFour.Length == 0)
+                    // 処理を返します
+                    return;
+                // 現在選択されている配列の座標を巡回地点の座標に代入
+                agent.destination = BranchpointsFour[bPoint].position;
+                bPoint = (bPoint + 1); //% BranchpointsFour.Length;
+                Debug.Log(bPoint);
+                break;
+            case 9:
+                if (BranchpointsOne.Length == 0)
+                    // 処理を返します
+                    return;
+                // 現在選択されている配列の座標を巡回地点の座標に代入
+                agent.destination = BranchpointsOne[bPoint].position;
+                bPoint = (bPoint + 1); //% BranchpointsOne.Length;
+                Debug.Log(bPoint);
+                break;
+            case 10:
+                if (BranchpointsTwo.Length == 0)
+                    // 処理を返します
+                    return;
+                // 現在選択されている配列の座標を巡回地点の座標に代入
+                agent.destination = BranchpointsTwo[bPoint].position;
+                bPoint = (bPoint + 1); //% BranchpointsTwo.Length;
+                Debug.Log(bPoint);
+                break;
+            case 12:
+                if (BranchpointsThree.Length == 0)
+                    // 処理を返します
+                    return;
+                // 現在選択されている配列の座標を巡回地点の座標に代入
+                agent.destination = BranchpointsThree[bPoint].position;
+                bPoint = (bPoint + 1); //% BranchpointsThree.Length;
+                Debug.Log(bPoint);
+                break;
+
+
+
+        }
+    }
+    void Branchpoint()
+    {
+        Branch = Random.value > 0.5f;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("AAAAAAAAA");
+        if (other.gameObject.CompareTag("BPoint"))
+        {
+            Branchpoint();
+            //Debug.Log(Branch);
+        }//Debug.Log("AAAAAAAAA");
+        if (other.gameObject.CompareTag("BPointEnd"))
+        {
+            //Debug.Log(destPoint);
+            switch (destPoint)
+            {
+                case 8:
+                    Debug.Log("8で０担ってます");
+                    bPoint = 0; destPoint = 14;
+                    Branch = false;
+                    break;
+                case 9:
+                    Debug.Log("９で０担ってます");
+                    bPoint = 0; destPoint = 9;
+                    Branch = false;
+                    break;
+                case 10:
+                    Debug.Log("10で０担ってます");
+                    bPoint = 0; destPoint = 13;
+                    Branch = false;
+                    break;
+                case 12:
+                    Debug.Log("12で0担ってます");
+                    bPoint = 0; destPoint = 13;
+                    Branch = false;
+                    break;
+            }
+
+        }
+    }
+    void ChaseTime()
+    {
+        Chasetime += Time.deltaTime;
+        if (Chasetime >= 5)
+        {
+            Debug.Log("５秒経過");
+            EnemyState = Enemy.Patrol;
+        }
+        else
+        {
+            EnemyState = Enemy.PlayerLook;
+        }
     }
 }
--- Assets/ヤマザキ/Script/MainGame/SetUpItem.cs
+++ Assets/ヤマザキ/Script/MainGame/SetUpItem.cs
@@ -10,9 +10,12 @@ public class SetUpItem : MonoBehaviour
     [SerializeField] GameObject Trap;
     [SerializeField] GameObject Light;
     [SerializeField] GameObject Wepon;
+    private String tag;
+    private GameObject SquareCorners;
     //[SerializeField] [Range(0f, 1f)] float capacity;
     int WeponC=1;
     int TrapC=3;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,40 +24,67 @@ public class SetUpItem : MonoBehaviour
         // array1をシャッフルする
         int[] array2 = array1.OrderBy(i => Guid.NewGuid()).ToArray();
 
+
         // 配置するアイテムの数
         int count = 10;
         // アイテム配置
         for (int n = 0; n < count; n++)
         {
-            //Debug.Log("AAAAAAAAAAAAAAAAAA");
-            if(WeponC>n)
-            { 
+            
+            //Debug.Log(transform.GetChild(array2[n]).gameObject.tag);
+            tag = transform.GetChild(array2[n]).gameObject.tag;
+            //SquareCorners = transform.GetChild(array2[n]).gameObject;
+            //SquareCorners.transform.Rotate(new Vector3(0, 180, 0));
+            //Debug.Log(SquareCorners.transform.Rotate);
+            if (WeponC>n)
+            {
+                //SquareCorners=Wepon;
+                Rotation();
                 Instantiate(Wepon, transform.GetChild(array2[n]).position, Wepon.transform.rotation);
-                //Debug.Log("W");
+                //Wepon.transform.Rotate(new Vector3(0, 180, 0));
             }
             else if(TrapC >= n)
             {
+                //SquareCorners = Trap;
+                Rotation();
                 Instantiate(Trap, transform.GetChild(array2[n]).position, Trap.transform.rotation);
+                //Trap.transform.Rotate(new Vector3(0, 180, 0));
                 //Debug.Log("T");
             }
             else
             {
+                //SquareCorners = Light;
+                Rotation();
                 Instantiate(Light, transform.GetChild(array2[n]).position, Light.transform.rotation);
+                //Light.transform.Rotate(new Vector3(0, 180, 0));
                 //Debug.Log("L");
             }
         }
         // 破壊
         Destroy(gameObject);
-        /*
-        int n = 0;
-        foreach (int i in ary2)
+        
+    }
+    void Rotation()
+    {
+       switch(tag)
         {
-            if (n < count)
-            {
-                Instantiate(item, transform.GetChild(i).position, item.transform.rotation);
-            }
-            Destroy(transform.GetChild(i).gameObject);
+            case "Up":
+                //SquareCorners.transform.Rotate(new Vector3(0, 270, 0));
+                Debug.Log("上だよー");
+                break;
+            case "Down":
+                //SquareCorners.transform.Rotate(new Vector3(0, 90, 0));
+                Debug.Log("sitaだよー");
+                break;
+            case "Left":
+                //SquareCorners.transform.Rotate(new Vector3(0, 180, 0));
+                Debug.Log("hidariだよー");
+                break;
+            case "Right":
+                //SquareCorners.transform.Rotate(new Vector3(0, 0, 0));
+                Debug.Log("migiだよー");
+                break;
+
         }
-        */
     }
 }
